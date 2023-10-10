a2b9fc8d7701e85f658aa90c01dd01dd765dbdb4
Andre Maier <andre.maier@stud.uni-bamberg.de>
2023-10-10 20:38:21 +0200

    districtrepository implementation for the method getDistrictsFromWarehouse() - need to find somehow batchread solution

--- wss-server-spring-aerospike/src/main/java/de/uniba/dsg/wss/data/access/DistrictRepositoryOperationsImpl.java
+++ wss-server-spring-aerospike/src/main/java/de/uniba/dsg/wss/data/access/DistrictRepositoryOperationsImpl.java
@@ -1,25 +1,56 @@
 package de.uniba.dsg.wss.data.access;
 
+import com.aerospike.client.BatchRead;
+import com.aerospike.client.Key;
+import com.aerospike.client.Record;
+import com.aerospike.client.policy.BatchPolicy;
 import com.aerospike.client.policy.WritePolicy;
 import de.uniba.dsg.wss.data.model.DistrictData;
+
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.aerospike.convert.MappingAerospikeConverter;
 import org.springframework.data.aerospike.core.AerospikeTemplate;
 
 public class DistrictRepositoryOperationsImpl implements DistrictRepositoryOperations {
 
   private final AerospikeTemplate aerospikeTemplate;
 
+  private MappingAerospikeConverter aerospikeConverter;
+
   @Autowired
-  public DistrictRepositoryOperationsImpl(AerospikeTemplate aerospikeTemplate) {
+  public DistrictRepositoryOperationsImpl(AerospikeTemplate aerospikeTemplate, MappingAerospikeConverter aerospikeConverter) {
     this.aerospikeTemplate = aerospikeTemplate;
+    this.aerospikeConverter = aerospikeConverter;
   }
 
+//TODO: HOW TO BATCH READ ?!
   @Override
   public List<DistrictData> getDistrictsFromWarehouse(List<String> districtRefsIds) {
-    return null;
+    List<DistrictData> districts = new ArrayList<>();
+
+    // Iterate over the district reference IDs and read each record individually
+    for (String id : districtRefsIds) {
+      // Read the record for the key
+      DistrictData districtData = aerospikeTemplate.findById(id, DistrictData.class);
+
+      // Check if the record exists
+      if (districtData != null) {
+        districts.add(districtData);
+      }
+    }
+
+    return districts;
   }
+
+
+
+
+
 
   @Override
   public void saveAll(Map<String, DistrictData> idsToDistricts) {
