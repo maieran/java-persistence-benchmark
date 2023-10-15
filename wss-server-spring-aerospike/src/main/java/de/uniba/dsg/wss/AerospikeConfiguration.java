bb1559612af129725efcbfc2c49d346f2fcc12ac
maieran <apak_mc@hotmail.de>
2023-10-15 18:55:24 +0200

    adding environment variables to aerospikeconfiguration

--- wss-server-spring-aerospike/src/main/java/de/uniba/dsg/wss/AerospikeConfiguration.java
+++ wss-server-spring-aerospike/src/main/java/de/uniba/dsg/wss/AerospikeConfiguration.java
@@ -3,22 +3,23 @@ package de.uniba.dsg.wss;
 import com.aerospike.client.Host;
 import java.util.Collection;
 import java.util.Collections;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.data.aerospike.config.AbstractAerospikeDataConfiguration;
 import org.springframework.data.aerospike.config.AerospikeDataSettings;
 import org.springframework.data.aerospike.repository.config.EnableAerospikeRepositories;
 
 /**
- * Configures the set-up for Aerospike data access in the corresponding repositories.
- * Facilitates the connection to the Aerospike database by defining the hosts and establishing
- * the namespace.
- * Provides the Aerospike Java client responsible for managing communication with
- * the underlying Aerospike server and its data model.
- * Provides also {@link AerospikeDataSettings} that enables scan functionality on secondary index that
- * is required in {@link de.uniba.dsg.wss.data.model.EmployeeData} for 'username' retrieve when accessing the terminal,
+ * Configures the set-up for Aerospike data access in the corresponding repositories. Facilitates
+ * the connection to the Aerospike database by defining the hosts and establishing the namespace.
+ * Provides the Aerospike Java client responsible for managing communication with the underlying
+ * Aerospike server and its data model. Provides also {@link AerospikeDataSettings} that enables
+ * scan functionality on secondary index that is required in {@link
+ * de.uniba.dsg.wss.data.model.EmployeeData} for 'username' retrieve when accessing the terminal,
  * that is implemented in {@link de.uniba.dsg.wss.data.access.EmployeeRepositoryOperationsImpl}.
- *
  *
  * @author Andre Maier
  */
@@ -26,22 +27,30 @@ import org.springframework.data.aerospike.repository.config.EnableAerospikeRepos
 @EnableAerospikeRepositories(basePackages = {"de.uniba.dsg.wss.data.access"})
 public class AerospikeConfiguration extends AbstractAerospikeDataConfiguration {
 
+  private final Environment environment;
+
+  @Autowired
+  public AerospikeConfiguration(Environment environment){
+    this.environment = environment;
+  }
+
   @Override
   protected Collection<Host> getHosts() {
-    return Collections.singleton(new Host("localhost", 3000));
+    return Collections.singleton(new Host(environment.getRequiredProperty("wss.aerospike.host"), Integer.parseInt(environment.getRequiredProperty("wss.aerospike.port"))));
   }
 
   @Override
   protected String nameSpace() {
-    return "test";
+    return (environment.getRequiredProperty("wss.aerospike.namespace"));
   }
 
   /**
-   * The {@link AerospikeDataSettings} object allows the fine-tuning of scan-related settings and other properties related to data access and manipulation.
-   * Enable scan operations for accessing and retrieving data from the Aerospike database.
+   * The {@link AerospikeDataSettings} object allows the fine-tuning of scan-related settings and
+   * other properties related to data access and manipulation. Enable scan operations for accessing
+   * and retrieving data from the Aerospike database.
    *
-   *
-   * @return An instance of {@link AerospikeDataSettings} that encapsulates the configuration settings for Aerospike data operations.
+   * @return An instance of {@link AerospikeDataSettings} that encapsulates the configuration
+   *     settings for Aerospike data operations.
    */
   @Bean
   public AerospikeDataSettings aerospikeDataSettings() {
